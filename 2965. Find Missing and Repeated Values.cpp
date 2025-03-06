bde4564e2fd8c8d6c0270b81689dee676818f00b
kushalmittal04 <162821581+kushalmittal04@users.noreply.github.com>
2025-03-06 16:48:31 +0530

    2965

---
+++ 2965. Find Missing and Repeated Values.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+class Solution {
+    public:
+        vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+            int n = grid.size();
+            int N = n*n;
+            int repeated = -1;
+            int missing = -1;
+    
+            unordered_map<int,int> freq;
+    
+            for(int i=0 ; i<n ; i++) {
+                for(int j=0 ; j<n ; j++) {
+                    // Code
+                    int val = grid[i][j];
+                    freq[val]++;
+                }
+            }
+    
+            
+            for (int i = 1; i <= N; i++) {
+                if (freq[i] == 2) repeated = i;
+                if (freq[i] == 0) missing = i;
+            }
+    
+            return {repeated, missing};
+        }
+    };
